66ca9ea376f31a2d04402d7f18548e021f22c536
Yigezu Mulugeta <Yigezumb@gmail.com>
2021-01-13 16:29:36 -0500

    updated code

--- client/src/components/BookWrapper/index.js
+++ client/src/components/BookWrapper/index.js
@@ -36,42 +36,6 @@ const BookWrapper = props => {
 
 }
 
-// class BookWrapper extends Component {
-
-//     state = {
-//         message : ""
-//     }
-
-//     saveArticle = book => {
-//         API.saveBook(book)
-//         this.setState({message:"This Book Has Been Saved! "})
-
-//     }
-
-
-// render(){
-//     const {authors,title,description,link,image} = this.props
-  
-//     return(
-//         <div className="book-wrapper">
-//             <div>
-//             <h1>{title} Written By : {authors.join(", ")}</h1>
-//             <h3>Synopsis : {description}</h3>
-           
-//             </div>
-//             <div>
-            
-//             <h2><img alt={title +"image"} src={image}></img></h2>
-//             <h2><a className="link" rel="noopener noreferrer" target="_blank" href={link}>Link to Book</a></h2>
-//             <h2><button className="save-btn" onClick={()=> this.saveArticle(this.props)}>Save Book</button></h2>
-//             <h2>{this.state.message}</h2>
-//             </div>
-//         </div>
-//     )
-// }
-
-// }
-
 
 
 export default BookWrapper
--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -18,7 +18,7 @@ const [savedbooks,setSavedBooks] = useContext(SavedBooksContext)
     useEffect(() => {
         findSavedBooks()
         
-      },[]);
+      });
 
     const findSavedBooks = async () =>{
         const {data:savedBooks} = await API.findSavedBooks()
@@ -67,49 +67,5 @@ const [savedbooks,setSavedBooks] = useContext(SavedBooksContext)
         )
 
 }
-
-// class Saved extends React.Component{
-
-//     state = {
-//         savedbooks: []
-//     }
-
-
-
-//     render(){
-//         return(
-//             <SearchWrapper>
-//             <div className="header-wrapper">
-//             <h2 className="header">Saved Books</h2>
-//             </div>
-            
-        
-//              <SearchResultsWrapper>
-//                 {this.state.savedbooks && this.state.savedbooks.length > 0 ? 
-                
-//                 this.state.savedbooks.map(book =>(
-//                     <SavedBookWrapper
-//                     key={book._id}
-//                     _id={book._id}
-//                     authors={book.authors ? book.authors : ["No Author Available"]}
-//                     title={book.title}
-//                     description={book.description ? book.description : "No Description Available"}
-//                     link={book.link}
-//                     image={book.image ? book.image : "#"}
-//                     handleClick={this.handleClick}
-                   
-//                      />
-//                 ))
-                
-//                 : <NoResults />}
-                 
-//             </SearchResultsWrapper>   
-      
-
-//             </SearchWrapper>
-            
-//         )
-//     }
-// }
 
 export default Saved
